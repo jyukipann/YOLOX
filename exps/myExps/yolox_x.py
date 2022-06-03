3aef63fcd1647cb5b07d65a5640a713825bff272
JUKI <jyukipann@yahoo.co.jp>
2022-06-03 18:09:32 +0900

    学習用パラメータ追加

--- exps/myExps/yolox_x.py
+++ exps/myExps/yolox_x.py
@@ -22,3 +22,19 @@ class Exp(MyExp):
             self.data_dir = "/work/tanimoto.j/dataset/flir/FLIR_ADAS_1_3/"
             self.train_ann = "train/thermal_annotations.json"
             self.val_ann = "val/thermal_annotations.json"
+        
+        # --------------  training config --------------------- #
+        self.warmup_epochs = 5
+        self.max_epoch = 115
+        self.warmup_lr = 0
+        self.basic_lr_per_img = 0.01 / 64.0
+        self.scheduler = "yoloxwarmcos"
+        self.no_aug_epochs = 15
+        self.min_lr_ratio = 0.05
+        self.ema = True
+
+        self.weight_decay = 5e-4
+        self.momentum = 0.9
+        self.print_interval = 10
+        self.eval_interval = 10
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
