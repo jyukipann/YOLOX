127f1542b19ea5ec4e198305942d47561f8bc2fe
JUKI <jyukipann@yahoo.co.jp>
2022-06-12 16:05:06 +0900

    result

--- myutil/compare_demo_result_my_result.py
+++ myutil/compare_demo_result_my_result.py
@@ -12,7 +12,8 @@ def opencsv(path):
 
 
 demo_result = r"experiment_result\demoOutput\rgb_result_demo.csv"
-my_result = r"experiment_result\demoOutput\20220607_flir_dataset_val_RGB_yolox_result.csv"
+# my_result = r"experiment_result\demoOutput\20220607_flir_dataset_val_RGB_yolox_result.csv"
+my_result = r"experiment_result\before\20220607_flir_dataset_val_RGB_yolox_iou_result_sunny.csv"
 
 demo_result_data = opencsv(demo_result)[1:]
 my_result_data = opencsv(my_result)[1:]
@@ -47,12 +48,12 @@ for i,(file_name,img_id,x1,y1,x2,y2,conf,cat) in enumerate(my_result_data):
 # print(id(demo_filename_counts[list(demo_filename_counts.keys())[1]]))
 # print(id(demo_filename_counts[file_name]))
 # exit(0)
-for file_name in file_names:
-    if len(demo_filename_counts[file_name]) != len(my_filename_counts[file_name]):
-        # print(file_name, len(demo_filename_counts[file_name]), len(my_filename_counts[file_name]))
-        # exit(0)
-        demo = demo_result_data[demo_filename_counts[file_name]]
-        my =  my_result_data[my_filename_counts[file_name]]
-        print(*demo)
-        print(*my)
-        # exit(0)
+# for file_name in file_names:
+#     if len(demo_filename_counts[file_name]) != len(my_filename_counts[file_name]):
+#         # print(file_name, len(demo_filename_counts[file_name]), len(my_filename_counts[file_name]))
+#         # exit(0)
+#         demo = demo_result_data[demo_filename_counts[file_name]]
+#         my =  my_result_data[my_filename_counts[file_name]]
+#         print(*demo)
+#         print(*my)
+#         # exit(0)
